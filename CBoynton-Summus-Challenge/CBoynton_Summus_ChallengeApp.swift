d23d86b24d32525efb732f2e7ea526ccd2dde96e
Christopher Boynton <cboynton16@gmail.com>
2022-08-25 14:00:10 -0400

    Initial Commit

---
+++ CBoynton-Summus-Challenge/CBoynton_Summus_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBoynton_Summus_ChallengeApp.swift
+//  CBoynton-Summus-Challenge
+//
+//  Created by Christopher Boynton on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CBoynton_Summus_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
